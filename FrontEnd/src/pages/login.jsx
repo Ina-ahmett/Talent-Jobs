bded36770b68da21b253f20dd289b3eba72222bd
yahye <yahyeahmedrubeey@gmail.com>
2025-03-22 22:07:29 +0300

    tjiaabo login

--- FrontEnd/src/pages/login.jsx
+++ FrontEnd/src/pages/login.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Header from "../components/header";
-import Footer from "../components/footer";
 import logo from "../assets/Talent-Jobs.png"; // Hubi in aad ku dartay image-ka logo
 import { Link, useNavigate } from "react-router-dom";
 
 const Login = (props) => {
+  //Login page waye waana tijabinaaa
     const navigate = useNavigate();
 
   return (
